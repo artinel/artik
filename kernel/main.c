cd50909c101c3302d4e1f0054c7d0552dc59fbfb
artinel <artinel@proton.me>
2025-03-04 09:49:34 +0330

    (kernel/main.c) : Remove tests

--- kernel/main.c
+++ kernel/main.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-#include <string.h>
-#include <kernel/tty.h>
-
-#define MY_NAME "Artinel"
-#define ADDR 0xB8000
-
-void kernel_main(void){
-	printk("Hello My name is %s and i am %x years old\n", MY_NAME, ADDR);	
-}
+void kernel_main(void){}
