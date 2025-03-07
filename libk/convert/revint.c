87d57ec5dcb5b4b20eac99e3347926fd5a5612ac
artinel <artinel@proton.me>
2025-03-07 11:50:14 +0330

    (libk/convert/revint.c) : Write document

--- libk/convert/revint.c
+++ libk/convert/revint.c
@@ -1,5 +1,21 @@
+/**
+* \file libk/convert/revint.c
+* \brief Reverse an integer
+* \author Artinel <artinel@proton.me>
+* \version 0.0.1
+* \date 2025-03-07
+* \copyright GNU Public License V3
+*/
+
 #include <convert.h>
 
+/**
+* Reverse an integer(456 -> 654 ane etc...).
+*
+* \param number The number that will be reversed
+*
+* \return revint returns the reversed number
+*/
 int revint(int number){
 	if(number < 0){
 		number = -number;
