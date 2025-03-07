2cb6b73bb21d77c1050e8fcbabdd6cfeb3b2b137
artinel <artinel@proton.me>
2025-03-07 12:10:15 +0330

    (libk/convert/xtos) : Write document

--- libk/convert/xtos.c
+++ libk/convert/xtos.c
@@ -1,5 +1,27 @@
+/**
+* \file libk/convert/xtos.c
+* \brief Convert hexadecimal to string
+* \author Artinel <artinel@proton.me>
+* \version 0.0.1
+* \date 2025-03-07
+* \copyright GNU Public License V3
+*/
+
+
 #include <convert.h>
 #include <stddef.h>
+
+/**
+* Convert hexadecimal to string and write it to buffer.
+*
+* \param number The hexadecimal number that will be converted to string
+* \param buffer The buffer that the converted number will be written on
+* \param size Size of the buffer
+*
+* \return xtos returns the number of bytes written to buffer
+*
+* \sa xtoc
+*/
 
 size_t xtos(int number, char* buffer, size_t size){
 	size_t i = 0;
