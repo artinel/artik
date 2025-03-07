e85b0c98a8b2c14c6488897663ab9c3aea23e3c3
artinel <artinel@proton.me>
2025-03-07 11:50:44 +0330

    (libk/convert/xtoc.c) : Write document

--- libk/convert/xtoc.c
+++ libk/convert/xtoc.c
@@ -1,4 +1,21 @@
+/**
+* \file libk/convert/xtoc.c
+* \brief Convert hexadecimal to character
+* \author Artinel <artinel@proton.me>
+* \version 0.0.1
+* \date 2025-03-07
+* \copyright GNU Public License V3
+*/
+
 #include <convert.h>
+
+/**
+* Convert hexadecimal to character(6 -> '6' , 10 -> 'A' and etc...).
+*
+* \param c The hexadecimal value that will be converted to character
+*
+* \return xtoc returns the converted character
+*/
 
 char xtoc(int c){
 	char* chars = "0123456789ABCDEF";
