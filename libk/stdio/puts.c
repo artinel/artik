1ea0c3493d9e3dbcdc83c3d1ab9237f1d5e35bf0
artinel <artinel@proton.me>
2025-03-08 17:43:22 +0330

    (libk/stdio/puts.c) : Write documents

--- libk/stdio/puts.c
+++ libk/stdio/puts.c
@@ -1,6 +1,25 @@
+/**
+* \file libk/stdio/puts.c
+* \brief Print a string on the screen
+* \author Artinel <artinel@proton.me>
+* \version 0.0.1
+* \date 2025-03-08
+* \copyright GNU Public License V3
+*/
+
 #include <stdio.h>
 #include <stddef.h>
 #include <string.h>
+
+/**
+* Print a string on the screen.
+*
+* \param str The string that will be printed on the screen
+*
+* \return puts returns the number of bytes printed on the screen
+*
+* \sa strlen putchar
+*/
 
 size_t puts(const char* str){
 	size_t written = 0;
