f187822083881c8711394051b1b8055effd7715a
artinel <artinel@proton.me>
2025-03-08 17:47:31 +0330

    (libk/string/strlen.c) : Write documents

--- libk/string/strlen.c
+++ libk/string/strlen.c
@@ -1,5 +1,22 @@
+/**
+* \file libk/string/strlen.c
+* \brief Returns the length of a string
+* \author Artinel <artinel@proton.me>
+* \version 0.0.1
+* \date 2025-03-08
+* \copyright GNU Public License V3
+*/
+
 #include <string.h>
 #include <stddef.h>
+
+/**
+* Returns the length of a string
+*
+* \param str The string that its size will be returned
+*
+* \return strlen returns the length of the string
+*/
 
 size_t strlen(const char* str){
         size_t len = 0;
