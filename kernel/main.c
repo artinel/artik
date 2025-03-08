6cf5a2b8a700f797e603e918d1ccc61fe8274eb3
artinel <artinel@proton.me>
2025-03-08 17:57:28 +0330

    (kernel/main.c) : Write documents

--- kernel/main.c
+++ kernel/main.c
@@ -1,1 +1,10 @@
+/**
+* \file kernel/main.c
+* \brief The main function of kernel
+* \author Artinel <artinel@proton.me>
+* \version 0.0.1
+* \date 2025-03-08
+* \copyright GNU Public License V3
+*/
+
 void kernel_main(void){}
