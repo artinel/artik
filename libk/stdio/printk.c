2e53bc315413449b48f35d2a3de5c8de3a431035
artinel <artinel@proton.me>
2025-03-08 17:29:53 +0330

    (libk/stdio/printk.c) : Write documents

--- libk/stdio/printk.c
+++ libk/stdio/printk.c
@@ -1,8 +1,28 @@
+/**
+* \file libk/stdio/printk.c
+* \brief Print formatted text on the screen
+* \author Artinel <artinel@proton.me>
+* \version 0.0.1
+* \date 2025-03-08
+* \copyright GNU Public License V3
+*/
+
 #include <stdio.h>
 #include <stddef.h>
 #include <stdarg.h>
 #include <string.h>
 #include <convert.h>
+
+/**
+* Print formatted text on the screen.
+*
+* \param str The output format string
+* \param ... The arguments for the format string
+*
+* \return printk returns the number of bytes printed on the screen
+*
+* \sa strlen puts itos xtos putchar
+*/
 
 size_t printk(const char* str, ...){
         va_list args;
