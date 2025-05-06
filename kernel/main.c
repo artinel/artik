dc3b2eb161d2d51ae0648b4021fd0dc964def314
artinel <artinel@proton.me>
2025-05-06 07:48:47 +0330

    A test for vga_scroll

--- kernel/main.c
+++ kernel/main.c
@@ -1,10 +1,34 @@
 #include <kernel/vga.h>
 
+static const char* hello = "Hello World!!";
+
+static void wait(){
+	for(int i = 0; i < 199900000; i++){}
+}
+
 void kernel_main(void){
 	vga_init();
-	vga_set_bg(VGA_COLOR_BLUE);
-	vga_set_fg(VGA_COLOR_GREEN);
-	for(int i = 0; i < 120; i++){
-		vga_putchar('M');
+	vga_set_fg(VGA_COLOR_BLUE);
+	vga_paint_bg(VGA_COLOR_WHITE);
+
+	for(uint8_t c = 'A'; c <= 'Z'; c++){
+		wait();
+		vga_putchar(c);
+		vga_putchar(c);
+
+		vga_putchar(c);
+		vga_putchar(c);
+		vga_putchar(c);
+		vga_putchar(c);
+		vga_putchar(c);
+		vga_putchar('\n');
 	}
+
+	for(uint8_t c = 'X'; c <= 'Z'; c++){
+		wait();
+		vga_putchar(c);
+		vga_putchar(c);
+		vga_putchar('\n');
+	}
+
 }
