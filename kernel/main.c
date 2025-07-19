38ab683443f880cb4b69a5072cd9b46463c7ca66
artinel <artinel@proton.me>
2025-07-19 07:18:24 +0330

    Print a version message instead of hello world

--- kernel/main.c
+++ kernel/main.c
@@ -1,11 +1,5 @@
 #include <video/vga.h>
 
-static const char* hello = "Hello World!!";
-
-static void wait(){
-	for(int i = 0; i < 199900000; i++){}
-}
-
 static uint32_t str_len(const char *str) {
 	uint32_t size = 0;
 	while (*str++) {
@@ -16,15 +10,15 @@ static uint32_t str_len(const char *str) {
 }
 
 void kernel_main(void){
+	const char* version = "Artik Version 0.0.1\n";
+	
 	vga_init();
 	vga_set_fg(VGA_COLOR_GREEN);
 	vga_paint_bg(VGA_COLOR_BLACK);
 
-	uint32_t len = str_len(hello);
-	int col = (VGA_MAX_COL / 2) - (len / 2);
-	int row = VGA_MAX_ROW / 2;
-
+	uint32_t len = str_len(version);
+	
 	for (uint32_t i = 0; i < len; i++) {
-		vga_putchar_at(hello[i], row, col++);
+		vga_putchar(version[i]);
 	}
 }
