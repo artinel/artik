6be1865a688d5bfd2cf77631fe34fbc8452bb627
artinel <artinel@proton.me>
2025-07-19 07:04:39 +0330

    Add disable_cursor function for disabling vga textmode cursor

--- arch/x86/driver/vga/cursor.c
+++ arch/x86/driver/vga/cursor.c
@@ -6,14 +6,14 @@
 #include "vga_regs.h"
 
 void enable_cursor(uint8_t cursor_start, uint8_t cursor_end) {
-	outb(VGA_CTRL_PORT, VGA_COLOR_PLANE_REG);
+	outb(VGA_CTRL_PORT, VGA_COLOR_PLANE);
 	outb(VGA_CTRL_WR_PORT, 
-			(inb(VGA_CTRL_WR_PORT) & VGA_CGA_REG)
+			(inb(VGA_CTRL_WR_PORT) & VGA_CGA)
 			| cursor_start);
 
-	outb(VGA_CTRL_PORT, VGA_MODE_CTRL_REG);
+	outb(VGA_CTRL_PORT, VGA_MODE_CTRL);
 	outb(VGA_CTRL_WR_PORT, 
-			(inb(VGA_CTRL_WR_PORT) & VGA_EXT_COLOR_REG)
+			(inb(VGA_CTRL_WR_PORT) & VGA_EXT_COLOR)
 			| cursor_end);
 
 }
@@ -22,6 +22,11 @@ void update_cursor(uint8_t row, uint8_t col) {
 	uint16_t pos = row * VGA_MAX_COL + col;
 	outb(VGA_CTRL_PORT, VGA_COLOR_PLANE_ENABLER);
 	outb(VGA_CTRL_WR_PORT, (uint8_t) (pos & 0xFF));
-	outb(VGA_CTRL_PORT, VGA_MEM_MODE_REG);
+	outb(VGA_CTRL_PORT, VGA_MEM_MODE);
 	outb(VGA_CTRL_WR_PORT, (uint8_t) ((pos >> 8) & 0xFF));
+}
+
+void disable_cursor() {
+	outb(VGA_CTRL_PORT, VGA_COLOR_PLANE);
+	outb(VGA_CTRL_WR_PORT, VGA_COLOR_PLANE_DISABLER);
 }
