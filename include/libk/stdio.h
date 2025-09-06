c6d50f407c214f7df799d9cfa89ad7d375d3bac5
artinel <artinel@proton.me>
2025-09-06 16:56:28 +0330

    Add putc and puts functions for printing a character and a string

---
+++ include/libk/stdio.h
@@ -0,0 +1,9 @@
+#ifndef __LIBK_STDIO_H_
+#define __LIBK_STDIO_H_
+
+#define EOF -1
+
+int putc(char);
+int puts(const char *);
+
+#endif
---
+++ libk/stdio.c
@@ -0,0 +1,25 @@
+#include <libk/stdio.h>
+#include <libk/stdlib.h>
+#include <video/tty.h>
+
+int putc(char c) {
+	if (tty_putchar(c) == RET_SUCCESS) {
+		return c;
+	}
+	return EOF;
+}
+
+int puts(const char *str) {
+	int index = 0;
+
+	while (str[index] != 0) {
+		if (putc(str[index]) == EOF) {
+			return EOF;
+		}
+		index++;
+	}
+
+	putc('\n');
+
+	return index;
+}
