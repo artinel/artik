39b05c7c9ba26810d3af5f9947b9e8fc676167fd
artinel <artinel@proton.me>
2025-09-14 09:24:26 +0330

    Add console_get_self function to get console data

--- include/io/console.h
+++ include/io/console.h
@@ -2,6 +2,23 @@
 #define __CONSOLE_H_
 
 #include <stdint.h>
+#include <font/psf.h>
+
+struct console{
+	uint32_t char_bg;	/* character background */
+	uint32_t char_fg;	/* character foreground */
+	uint32_t scr_bg;	/* screen background */
+	uint16_t width;		/* screen width */
+	uint16_t height;	/* screen height */
+	uint16_t max_col;	/* maximum number of characters in a row */
+	uint16_t max_row;	/* maximus number of characters in a column */
+	uint16_t col;		/* current col position */
+	uint16_t row;		/* current row position */
+	uint32_t scanline;	/* bytes to pixels */
+	psf2_t *font;		/* a pointer to the psf font data */
+	uint16_t cursor_col;	/* current cursor x position*/
+	uint16_t cursor_row;	/* current cursor y position */
+};
 
 void init_console(void);
 void console_putchar(uint16_t ch);
@@ -10,5 +27,6 @@ void console_set_background(uint32_t bg);
 void console_set_foreground(uint32_t fg);
 void console_paint_background(uint32_t bg);
 void console_putchar_at(uint16_t ch, uint16_t col, uint16_t row);
+struct console* console_get_self(void);
 
 #endif
--- src/io/console.c
+++ src/io/console.c
@@ -10,22 +10,6 @@
 #define PIXEL uint32_t
 #define TAB_SIZE 8
 
-struct console{
-	uint32_t char_bg;	/* character background */
-	uint32_t char_fg;	/* character foreground */
-	uint32_t scr_bg;	/* screen background */
-	uint16_t width;		/* screen width */
-	uint16_t height;	/* screen height */
-	uint16_t max_col;	/* maximum number of characters in a row */
-	uint16_t max_row;	/* maximus number of characters in a column */
-	uint16_t col;		/* current col position */
-	uint16_t row;		/* current row position */
-	uint32_t scanline;	/* bytes to pixels */
-	psf2_t *font;		/* a pointer to the psf font data */
-	uint16_t cursor_col;	/* current cursor x position*/
-	uint16_t cursor_row;	/* current cursor y position */
-};
-
 static struct console console;
 
 static void console_draw_cursor(void);
@@ -244,4 +228,8 @@ void console_putchar_at(uint16_t ch, uint16_t col, uint16_t row) {
 	console.col = col;
 	console.row = row;
 	console_putchar(ch);
+}
+
+struct console* console_get_self(void) {
+	return &console;
 }
