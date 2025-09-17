c0b1cae6fd384e9e2fbe32ec9ee48e59dbebf661
artinel <artinel@proton.me>
2025-09-17 17:46:13 +0330

    Implement strcmp function for comparing two strings

--- include/libk/string.h
+++ include/libk/string.h
@@ -5,5 +5,6 @@
 
 void *memmove(void *dest, const void *src, size_t n);
 void *memset(void *dest, int c, size_t count);
+int strcmp(const char *s1, const char *s2);
 
 #endif
--- src/libk/string.c
+++ src/libk/string.c
@@ -28,3 +28,18 @@ void *memset(void *dest, int c, size_t count) {
 
 	return dest;
 }
+
+int strcmp(const char *s1, const char *s2) {
+	while (*s1 == *s2 && *s1 != 0 && *s2 != 0) {
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2) {
+		return 0;
+	} else if (*s1 > *s2) {
+		return 1;
+	} else {
+		return -1;
+	}
+}
