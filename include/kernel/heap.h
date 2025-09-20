209ddc68ec608ea2ad871bfa9e2bb1ce92d18b46
artinel <artinel@proton.me>
2025-09-20 17:42:35 +0330

    Implement heap_alloc function to allocate dynamic memory

--- include/kernel/heap.h
+++ include/kernel/heap.h
@@ -3,16 +3,22 @@
 
 #include <stdint.h>
 
-#define HEAP_FREE	0x01
+#define HEAP_FREE		0x01
+
+#define HEAP_ALLOC_OK		0x00
+#define HEAP_ALLOC_INVL_INDEX	0x01
+#define HEAP_ALLOC_FAIL		0x02
 
 struct heap_header {
 	void *prev_header;
 	uint8_t flags;
+	uint16_t size;
 	void *next_header;
 };
 
 typedef struct heap_header heap_header_t;
 
 void init_heap(void);
+void *heap_alloc(uint16_t size);
 
 #endif
--- src/kernel/heap.c
+++ src/kernel/heap.c
@@ -1,30 +1,103 @@
 #include <kernel/heap.h>
 #include <kernel/pm_manager.h>
 #include <kernel/vm_manager.h>
+#include <kernel/paging.h>
 #include <libk/flags.h>
 #include <stdint.h>
 #include <stdlib.h>
 
-static void *pages[32];
+#define TOTAL_PAGES	32
+
+static void *pages[TOTAL_PAGES];
 static uint8_t pages_count = 0;
+static uint8_t header_size = 0;
+
+static uint8_t heap_alloc_page(uint8_t index);
 
 void init_heap(void) {
-	void *init_page = pm_alloc_page();
+	header_size = header_size;
 
-	/* no page found to allocated */
-	if (init_page == NULL) {
+	uint8_t res = heap_alloc_page(0);
+	
+	if (res != HEAP_ALLOC_OK) {
 		/* [TODO] panic the kernel*/
 		return;
 	}
-	
-	/* Add the initial page virtual address to the pages array */
-	init_page = vm_phys_to_virt(init_page);
-	pages[0] = init_page;
+}
+
+static uint8_t heap_alloc_page(uint8_t index) {
+	if (index >= TOTAL_PAGES) {
+		return HEAP_ALLOC_INVL_INDEX;
+	}
+
+	if (pages[index] != 0) {
+		return HEAP_ALLOC_INVL_INDEX;
+	}
+
+	void *page = pm_alloc_page();
+
+	if (page == NULL) {
+		return HEAP_ALLOC_FAIL;
+	}
+
+	page = vm_phys_to_virt(page);
+	pages[index] = page;
 	pages_count++;
 
-	/* Set the base header */
-	heap_header_t *base_header = (heap_header_t *) init_page;
+	heap_header_t *base_header = (heap_header_t *) page;
 	base_header->prev_header = NULL;
 	base_header->next_header = NULL;
 	SET_FLAG(base_header->flags, HEAP_FREE);
+	base_header->size = PAGE_SIZE - header_size;
+	
+	return HEAP_ALLOC_OK;
+}
+
+void *heap_alloc(uint16_t size) {
+	if (size > PAGE_SIZE - header_size) {
+		return NULL;
+	}
+	
+	heap_header_t *header = NULL;
+	for (uint8_t i = 0; i < pages_count; i++) {
+		header = pages[i];
+
+		while (header != NULL) {
+			if (CHECK_FLAG(header->flags, HEAP_FREE)) {
+				if (header->size == size) {
+					UNSET_FLAG(header->flags, HEAP_FREE);
+					return header + header_size;
+				}
+
+				if (header->size >= size + header_size) {
+					UNSET_FLAG(header->flags, HEAP_FREE);
+					heap_header_t *h = (heap_header_t *)(header + 
+						header_size + size);
+
+					h->prev_header = (void *) header;
+					h->next_header = header->next_header;
+					header->next_header = (void *) h;
+					if (h->next_header != NULL) {
+						heap_header_t *tmp = 
+							(heap_header_t*) h->next_header;
+						tmp->prev_header = (void *) h;
+					}
+					SET_FLAG(h->flags, HEAP_FREE);
+					h->size = header->size - (size + header_size);
+					header->size = size;
+
+					return header + header_size;
+				}
+			}
+			header = header->next_header;
+		}
+
+		if ((i + 1) == pages_count) {
+			if (heap_alloc_page(i + 1) != HEAP_ALLOC_OK) {
+				return NULL;
+			}
+		}
+	}
+
+	return NULL;
 }
